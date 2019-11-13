f2d59ba5350b3a2dec4403c14f75fb93fae9ef16
Cyril Carral <cyril.carral85@gmail.com>
2019-11-13 19:10:42 +0100

    react12 done

--- src/App.js
+++ src/App.js
@@ -13,21 +13,30 @@ class App extends Component {
     this.state = {
       activeId: 'home'
     };
+    this.handleChangeTab = this.handleChangeTab.bind(this);
+  }
+
+  handleChangeTab(event) {
+    const buttonId = event.target.id;
+    this.setState({ activeId: buttonId });
   }
 
   getTabContent() {
     switch(this.state.activeId) {
       case 'home':
         return <Home />;
-      default:
-        return <Error />;
+      case 'about':
+        return <About />
+      case 'contact':
+        return <Contact />
+      
     }
   }
 
   render() {
     return (
       <div className="App">
-        <TabSelector />
+        <TabSelector activeId={this.state.activeId} handleChangeTab={this.handleChangeTab}/>
         <div className="App-content">
           {
             this.getTabContent()
